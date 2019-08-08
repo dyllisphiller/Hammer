ccd8109f6e038bd04a7d54afaca5b186eeeda3c1
Dylan Umsted <dylan@umsted.org>
2019-08-08 16:34:28 -0700

    Document MainPage.xaml.cs better

--- CallsignLookup/MainPage.xaml.cs
+++ CallsignLookup/MainPage.xaml.cs
@@ -17,7 +17,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Newtonsoft.Json.Linq;
 
-// Project originated from Microsoft's Blank Page item template https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// Project started with Microsoft's Blank Page template https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace CallsignLookup
 {
@@ -28,29 +28,36 @@ namespace CallsignLookup
     public sealed partial class MainPage : Page
     {
         // The endpoint of the callsign lookup API
-        // calllook.info is pretty awesome with their API terms
-        // So they're pretty awesome, and if they're reading this: thanks!
+        // callook.info is pretty awesome with their API terms
+        // If anyone from there is reading this: thanks!
+        // MUST end with slash, as it's used as HttpClient.BaseAddress
         const string EndpointURL = "https://callook.info/";
 
         private async Task RetrieveData()
         {
+            // Instantiate an HttpClient
             var client = new HttpClient();
+
+            // Initialize variables
             JObject jResult = null;
             string CallsignTextLowercase = CallsignTextBox.Text.ToLower();
-
             string ResponseContent = null;
             string CallsignStatus = null;
 
+            // Reset result fields
             LookupResultTextBlock.Text = "";
 
+            // Construct the HTTP request parameters
             string URLParams = $"{CallsignTextLowercase}/json";
 
+            // Set the HttpClient instance's base address
             client.BaseAddress = new Uri(EndpointURL);
+
+            // Make it clear that we really do want JSON.
+            // This might be omitted; API requests with /json return JSON
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
             HttpResponseMessage response = client.GetAsync(URLParams).Result;
-
-            
 
             if (response.IsSuccessStatusCode)
             {
@@ -58,7 +65,11 @@ namespace CallsignLookup
                 {
                     ResponseContent = await response.Content.ReadAsStringAsync();
                     jResult = JObject.Parse(ResponseContent);
+
+                    // Assign variables from JSON values
                     CallsignStatus = (string)jResult["status"];
+
+                    // Display the results in their fields
                     LookupResultTextBlock.Text = CallsignStatus;
                 }
                 catch(Exception ex)
