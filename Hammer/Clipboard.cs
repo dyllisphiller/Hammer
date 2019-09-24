e0a49691dd56e251605b1b7fa905b7ebde87b2d1
Dylan Umsted <dylan@umsted.org>
2019-09-24 13:14:02 -0700

    Add class for handling copying to clipboard.

---
+++ Hammer/Clipboard.cs
@@ -0,0 +1,14 @@
+﻿using Windows.ApplicationModel.DataTransfer;
+
+namespace Hammer
+{
+    class Clipboard
+    {
+        private readonly DataPackage dataPackage = new DataPackage();
+        public void SetContent(string text)
+        {
+            dataPackage.RequestedOperation = DataPackageOperation.Copy;
+            dataPackage.SetText(text);
+        }
+    }
+}
