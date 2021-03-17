d3247786ae7839f8a30e7568a19acbbf7fb917c6
Dylan Umsted <dylan@umsted.org>
2021-03-17 02:06:27 -0700

    Get things working again

--- Hammer/App.xaml.cs
+++ Hammer/App.xaml.cs
@@ -40,7 +40,7 @@ namespace Hammer
         /// <param name="e">Details about the launch request and process.</param>
         protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
-            Frame rootFrame = Window.Current.Content as Frame;
+            Frame rootFrame = (Frame)Window.Current.Content;
 
             // Do not repeat app initialization when the Window already has content,
             // just ensure that the window is active
--- Hammer/Properties/AssemblyInfo.cs
+++ Hammer/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Hammer")]
-[assembly: AssemblyCopyright("Copyright © 2019")]
+[assembly: AssemblyCopyright("Copyright © 2019-2021 Dylan Umsted")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
