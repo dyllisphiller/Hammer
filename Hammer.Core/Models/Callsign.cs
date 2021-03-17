90324d167ede797e593de17fbdbbd369358da4a8
Dylan Umsted <dylan@umsted.org>
2021-03-17 02:28:15 -0700

    Create a stub Callsign class for future data type enforcement

---
+++ Hammer.Core/Models/Callsign.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hammer.Core.Models
+{
+    public class Callsign
+    {
+        public string Sign { get; private set; }
+        public Callsign()
+        {
+
+        }
+
+        public Callsign(string sign)
+        {
+            Sign = sign;
+        }
+    }
+}
