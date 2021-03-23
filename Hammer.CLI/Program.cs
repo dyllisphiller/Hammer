fd2ce0d2f4a8d9cc9e7c38a35660c359a2c13a38
Dylan Umsted <dylan@umsted.org>
2021-03-23 16:08:38 -0700

    Create CLI project

---
+++ Hammer.CLI/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using Hammer.Core.Models;
+
+namespace Hammer.CLI
+{
+    class Program
+    {
+        enum Commands
+        {
+            None,
+            Lookup
+        }
+
+        static void Main(string[] args)
+        {
+            bool isSilent = args.Contains("-s");
+            Commands command;
+            string callsign;
+
+            Console.Write("Hammer CLI → ");
+
+            if (args.Contains("-l") || args.Contains("--lookup"))
+            {
+                if (!string.IsNullOrWhiteSpace(args.ElementAtOrDefault(1)))
+                {
+                    callsign = args.ElementAtOrDefault(1);
+                    command = Commands.Lookup;
+                    Lookup(callsign);
+                }
+                else
+                {
+                    Console.WriteLine("Lookups require a callsign.");
+                    return;
+                }
+            }
+            else command = Commands.None;
+
+            if (command == Commands.None)
+            {
+                Console.WriteLine("Help");
+                Console.WriteLine("Hammer provides callsign lookups.");
+                Console.WriteLine("To use Hammer, specify -l and a callsign.");
+                Console.WriteLine();
+                Console.WriteLine("EXAMPLES");
+                Console.WriteLine("\tHammer.exe -l W1AW");
+            }
+        }
+
+        static void Lookup(string callsign)
+        {
+            Console.WriteLine($"Callsign Lookup → {callsign}\n");
+        }
+    }
+}
