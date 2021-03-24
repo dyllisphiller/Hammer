9c4c0c8010c6afbed70ce267954b8e213d5d9a42
Dylan Umsted <dylan@umsted.org>
2021-03-24 16:49:50 -0700

    Start scaffolding AutoSuggest Helper

---
+++ Hammer/Helpers/AutoSuggest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hammer.Helpers
+{
+    public static class AutoSuggest
+    {
+        public static void AddSuggestion(this IList<string> list, string suggestion)
+        {
+            if (string.IsNullOrWhiteSpace(suggestion))
+            {
+
+            }
+        }
+    }
+}
