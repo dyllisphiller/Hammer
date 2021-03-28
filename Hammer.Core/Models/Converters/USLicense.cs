4c2abee0ee1082391be1b90387184c510e867985
Dylan Umsted <dylan@umsted.org>
2021-03-28 00:48:04 -0700

    Build out extension method and place classes before enums

--- Hammer.Core/Models/Converters/USLicense.cs
+++ Hammer.Core/Models/Converters/USLicense.cs
@@ -7,51 +7,77 @@ using System.Threading.Tasks;
 
 namespace Hammer.Core.Models.Converters.US
 {
-	public enum USStatus
-    {
-		Valid,
-		Invalid,
-		Updating,
-    }
-
-	public enum USLicenseType
-    {
-		Person,
-		Club,
-		Recreation,
-		Races,
-		Military,
-    }
-
-	public enum USOperatorClass
-    {
-		Novice,
-		Technician,
-		TechnicianPlus,
-		General,
-		Advanced,
-		Extra,
-    }
-
     public class USLicense
     {
-		public USStatus Status { get; set; }
-		public USLicenseType Type { get; set; }
-		public USLicenseData Current { get; set; }
-		public USLicenseData Previous { get; set; }
-		public USTrusteeData Trustee { get; set; }
-		public string Name { get; set; }
-		public USAddress Address { get; set; }
-		public USLocation Location { get; set; }
-		public USOtherInfo OtherInfo { get; set; }
+        public USStatus Status { get; set; }
+        [JsonPropertyName("type")]
+        public USLicenseType LicenseType { get; set; }
+        public USLicenseData Current { get; set; }
+        public USLicenseData Previous { get; set; }
+        public USTrusteeData Trustee { get; set; }
+        public string Name { get; set; }
+        public USAddress Address { get; set; }
+        public USLocation Location { get; set; }
+        public USOtherInfo OtherInfo { get; set; }
+        [JsonExtensionData]
+        public Dictionary<string, object> ExtensionData { get; set; }
     }
 
-	public static partial class ConverterExtensions
+    public static partial class ConverterExtensions
     {
-		public static async Task<License> ToLicenseAsync(this USLicense usLicense)
+        public static License ToLicense(this USLicense usLicense)
         {
-			throw new NotImplementedException();
+            License license = new License()
+            {
+                AddressAttn = usLicense.Address.Attn,
+                AddressLine1 = usLicense.Address.Line1,
+                AddressLine2 = usLicense.Address.Line2,
+                Callsign = usLicense.Current.Callsign,
+                Country = "us",
+                ExpiryDate = usLicense.OtherInfo.ExpiryDate,
+                FRN = usLicense.OtherInfo.FRN,
+                GrantDate = usLicense.OtherInfo.GrantDate,
+                GridSquare = usLicense.Location.GridSquare,
+                LastActionDate = usLicense.OtherInfo.LastActionDate,
+                LicenseType = usLicense.LicenseType.ToString(),
+                Name = usLicense.Name,
+                OperatorClass = usLicense.Current.OperClass.ToString(),
+                Trustee = new License()
+                {
+                    Callsign = usLicense.Trustee.Callsign,
+                    Name = usLicense.Trustee.Name,
+                },
+                UlsUri = usLicense.OtherInfo.UlsUri,
+            };
+
+            return license;
         }
+    }
+
+    public enum USStatus
+    {
+        Valid,
+        Invalid,
+        Updating,
+    }
+
+    public enum USLicenseType
+    {
+        Person,
+        Club,
+        Recreation,
+        Races,
+        Military,
+    }
+
+    public enum USOperatorClass
+    {
+        Novice,
+        Technician,
+        TechnicianPlus,
+        General,
+        Advanced,
+        Extra,
     }
 
     public struct USLicenseData
