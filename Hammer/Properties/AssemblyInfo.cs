8c0dce86413043d9c2a68ab380d1af72581f2097
Dylan Umsted <dylan@umsted.org>
2021-04-12 18:25:35 -0700

    Change app description

--- Hammer/Properties/AssemblyInfo.cs
+++ Hammer/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Hammer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Amateur radio utilities")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Hammer")]
