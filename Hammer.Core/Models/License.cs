5ffd82b45b408215a6bb3dcb0a8564b18add618d
Dylan Umsted <dylan@umsted.org>
2021-04-15 00:43:00 -0700

    Remove unused usings

--- Hammer.Core/Models/License.cs
+++ Hammer.Core/Models/License.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 using System.Text.Json.Serialization;
 using Hammer.Core.Maps;
 
