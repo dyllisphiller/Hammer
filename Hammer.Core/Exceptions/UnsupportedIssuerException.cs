4cc885ab41f85950fb90935f52e950c2bc9e81a2
Dylan Umsted <dylan@umsted.org>
2021-04-17 09:31:13 -0700

    Create UnsupportedIssuerException.cs

---
+++ Hammer.Core/Exceptions/UnsupportedIssuerException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Hammer.Core
+{
+    [Serializable]
+    public class UnsupportedIssuerException : Exception
+    {
+        public UnsupportedIssuerException()
+        {
+        }
+
+        public UnsupportedIssuerException(string message) : base(message)
+        {
+        }
+
+        public UnsupportedIssuerException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected UnsupportedIssuerException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
