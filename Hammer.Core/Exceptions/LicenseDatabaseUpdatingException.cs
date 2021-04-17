540df8405668f2becf2363ffbee09cf32369b831
Dylan Umsted <dylan@umsted.org>
2021-04-17 09:31:06 -0700

    Create LicenseDatabaseUpdatingException.cs

---
+++ Hammer.Core/Exceptions/LicenseDatabaseUpdatingException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Hammer.Core
+{
+    [Serializable]
+    public class LicenseDatabaseUpdatingException : Exception
+    {
+        public LicenseDatabaseUpdatingException()
+        {
+        }
+
+        public LicenseDatabaseUpdatingException(string message) : base(message)
+        {
+        }
+
+        public LicenseDatabaseUpdatingException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected LicenseDatabaseUpdatingException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
